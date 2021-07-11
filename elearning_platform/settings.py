17b64ba818376cc3297fbd9a54b15790f454d71d
nikhilchandra9852 <69288916+nikhilchandra9852@users.noreply.github.com>
2021-07-12 00:15:33 +0530

    Add files via upload

--- elearning_platform/settings.py
+++ elearning_platform/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wqxjl88yrh64c4wl80eqr+fd(za5@*m5772cwz5**uy9bwlidk
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','elearningvrsec.herokuapp.com']
 
 
 # Application definition
