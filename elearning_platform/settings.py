2ef4a1a08952cfe623f66cd7ea3ea1aaf283e503
nikhilchandra9852 <69288916+nikhilchandra9852@users.noreply.github.com>
2021-07-12 07:23:22 +0530

    Add files via upload

--- elearning_platform/settings.py
+++ elearning_platform/settings.py
@@ -202,5 +202,5 @@ MEDIA_URL = '/media/'
 STATIC_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static-only")
 MEDIA_ROOT=os.path.join(os.path.dirname(BASE_DIR),"static","media")
 STATICFILES_DIRS=[
-os.path.join(os.path.dirname(BASE_DIR), "static")
+os.path.join(os.path.dirname(BASE_DIR), "users/static")
 ]
